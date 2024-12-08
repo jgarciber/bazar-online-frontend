f9535530d7e6a28053aabb9bfed4afc22ca97a0e
jesusgarciber <jesusgarciber@gmail.com>
2024-12-08 12:32:11 +0100

    Modifico vite.config.js para que acepte las variables de entorno de azure en tiempo de compilación

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,5 +11,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  define: {
+    // Leer la variable de entorno VITE_HOST desde process.env en el entorno de compilación
+    // Si no está definida, tomará el valor por defecto 'http://localhost:3000'
+    'import.meta.env.VITE_HOST': JSON.stringify(
+      process.env.VITE_HOST || 'http://localhost:3000'
+    )
   }
 })
