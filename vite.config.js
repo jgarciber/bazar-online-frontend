7e3ab0855193be27f988c83c96777eb19de67c2b
jesusgarciber <jesusgarciber@gmail.com>
2024-12-10 22:19:46 +0100

    Arreglo el bug que no dejaba hacer scroll horizontal en la tabla de productos

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
     // Leer la variable de entorno VITE_HOST desde process.env en el entorno de compilación
     // Si no está definida, tomará el valor por defecto 'http://localhost:3000'
     //
+
+    // Descomentar para hacer el build (npm run build)
     'import.meta.env.VITE_HOST': JSON.stringify(
     //   process.env.VITE_HOST || 'http://localhost:3000'
       process.env.VITE_HOST || 'https://bazar-online-back-2-g9hfc8bbhxepetes.spaincentral-01.azurewebsites.net'
